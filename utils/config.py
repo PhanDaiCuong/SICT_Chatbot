5ac836b476c48730fd81e9e57a52090ae5092400
Phan Dai Cuong <phancuongtmtlhp@gmail.com>
2026-01-11 12:03:55 +0700

    add config data

---
+++ utils/config.py
@@ -0,0 +1,159 @@
+PATH_MAPPING = {
+    #Level 1: Truong
+    "sict_corpus":"Trường Công nghệ thông tin và Truyền thông (SICT)",
+    "seee_corpus": "Trường Điện - Điện tử (SEEE)",
+    "smae_corpus": "Trường Cơ khí - Ô tô (SMAE)",
+    
+    #Level 2: Categori 1
+    "gioithieu": "Giới thiệu",
+    "daotao": "Đào tạo",
+    "khoa": "Khoa",
+    "khoahoc_congnghe":"Khoa học - Công nghệ",
+    "phong_trungtam":"Phòng - Trung tâm",
+    "tintuc":"Tin Tức",
+    "tuyensinh":"Tuyển sinh",
+    "hoptacphattrien": "Hợp tác phát triển",
+    "sinhvien": "Sinh Viên",
+    
+    #Level 3: Categori 2
+    
+    ## Dao tao
+    "daihoc": "Đại học",
+    "saudaihoc": "Sau đại học",
+    "kehoach": "Kế hoạch",
+    "quyche": "Quy chế",
+    "bangtienanh": "Đào tạo bằng tiếng anh",
+    "daotaonganhan": "Đào tạo ngắn hạn",
+    "kysu": "Kỹ sư",
+    "daotaolienket": "Đào tạo liên kết",
+    
+    ## Gioi thieu
+    "cocautochuc": "Cơ cấu tổ chức",
+    "lienhe": "Liên hệ",
+    "thongdiepcuahieutruong": "Thông điệp của hiệu trưởng",
+    "tongquan": "Tổng quan",
+    "canbogianvien": "Cán bộ giảng viên",
+    "thongtinchung": "Thông tin chung",
+    "cosovatchat": "Cơ sở vật chất",
+    "chienluocphattrien": "Chiến lược phát triển",
+    
+    ## Hop tac phat trien
+    "hoptacdoanhnghiep": "Hợp tác doanh nghiệp",
+    "hoptacquocte": "Hợp tác quốc tế",
+    
+    ## Khoa học công nghệ
+    "ctkhcn": "Công trình khoa học công nghệ",
+    "ttkhcn": "Thông tin khoa học công nghệ",
+    "congtrinhcongbo": "Công trình công bố",
+    "detaiduan": "Đề tài dự dán",
+    "sv_nckh": "Sinh viên Nghiên cứu khoa học",
+    "tin_nckh": "Tin Nghiên cứu khoa học",
+    "nhomnghiencuu": "Nhóm nghiên cứu",
+    
+    
+    ## Phong/Trung Tâm
+    "hanhchinhtonghop": "Hành chính tổng hợp",
+    "phongdaotaokhcncongtacsinhvien": "Phòng đào tạo - KHCN và Cộng tác sinh viên",
+    "tt_dt_boiduongvahoptacphattrien": "Trung tâm đào tạo Bồi dưỡng và hợp tác phát triển",
+    "tt_nghiencuucongnghesxtm": "Trung tâm Ngiên cứu công nghệ và SXTM",
+    "tt_thuchanhvathucnghiemcokhi": "Trung tâm thực hành, Thực nghiệm cơ khí",
+    "tt_thuchanhvathucnghiemoto": "Trung tâm thực hành và thực nghiệm ô tô",
+    "tt_hoptacphattrien": "Trung tâm hợp tác phát triển",
+    "tt_nc&ungdungttnt": "Trung tâm nghiên cứu và ứng dụng trí tuệ nhân tạo",
+    "tt_nc_cgcn_ddt": "Trung tâm nghiên cứu và chuyển giao công nghệ điện - điện tử",
+    "tt_th_ddt": "Trung tâm thực hành điện - điện tử",
+    
+    ##Khoa
+    "khoa_congnghephanmem": "Khoa Công nghệ phần mềm",
+    "khoa_congnghethongtin": "Khoa Công nghệ thông tin",
+    "khoa_khoahocmaytinh": "Khoa Khoa học máy tính",
+    "khoa_mangmaytinhvatruyenthong": "Khoa Mạng máy tính và truyền thông",
+    "kd": "Khoa điện",
+    "kdt": "Khoa điện tử",
+    "khtn&vm": "Khoa hệ thống nhúng và vi mạch",
+    "kktnl": "Khoa kĩ thuật năng lượng",
+    "tdh": "Khoa tự động hóa",
+    
+    ##Sinh viên
+    "bieumau": 'Biểu mẫu',
+    "hoatdongcuusinhvien": "Hoạt động cựu sinh viên",
+    "hotrosinhvien": "Hỗ trợ sinh viên",
+    "tindoan_hoi": "Tin đoàn Hội",
+    "vieclam": "Việc làm",
+
+    #Level 4: Categori 3
+    
+    ## Ngành đào tạo đại học
+    "cnkt_ddt": "Ngành Công nghệ kĩ thuật điện, điện tử",
+    "cnkt_dtvt": "Ngành Công nghệ kĩ thuật điện tử - viễn thông",
+    "cnkt_dtys": "Ngành Công nghệ kĩ thuật điện tử y sinh",
+    "cnkt_mt": "Ngành Công nghệ kĩ thuật máy tính",
+    "cnkt_nhiet": "Ngành Công nghệ kĩ thật nhiệt",
+    "ktsxtm": "Ngành Kỹ thuật sản xuất thông minh",
+    "cnkt_dktdh": "Ngành Điều khiển và tự động hóa",
+    "nltt": "Ngành Năng lượng tái tạo",
+    "antoanthongtin": "Ngành An toàn thông tin",
+    "congnghedaphuongtien":"Ngành Công nghệ đa phương tiện",
+    "hethongthongtin": "Ngành Hệ thống thông tin",
+    "kithuatphanmem": "Ngành Kỹ thuật phần mềm",
+    "congnghethongtin": "Ngành Công nghệ thông tin",
+    "khoahocmanhtinh": "Ngành Khoa học máy tính",
+    "cnkt_cdt": "Ngành Công nghệ kĩ thật cơ điện tử",
+    "cnkt_codientuoto": "Ngành Công nghệ kĩ thuật cơ điện tử ô tô",
+    "cnkt_htcn": "Ngành Công nghệ kĩ thuật hệ thống công nghiệp",
+    "cnkt_km": "Ngành Công nghệ kĩ thuật khuôn mẫu",
+    "cnkt_ck": "Ngành Công nghệ kĩ thuật cơ khí",
+    "cnkt_oto": "Ngành Công nghệ kĩ thuật ô tô",
+    "robot&ttnt": "Ngành Robot và Trí tuệ nhân tạo",
+    "tkck&dkcn": "Ngành Thiết kế cơ khí và kiểu dáng công nghiệp",
+    "kt_ckdl": "Ngành Kỹ thuật cơ khí động lực",
+    
+    ## Sau đại học 
+    "thacsi": "Bậc Thạc sĩ",
+    "tiensi": "Bậc Tiến sĩ",
+    
+    
+    "bgh": "Ban giám hiệu",
+    "dubp": "Đảng ủy bộ phận",
+    "hdkhdt": "Hội đồng khoa học đào tạo",
+    "tochucdoanthe": "Tổ chức đoàn thể",
+    "sodotochuc": "Sơ đồ tổ chức",
+    
+}
+# 2. CẤU HÌNH PHÂN LOẠI (CLASSIFICATION_SETS)
+CLASSIFICATION_SETS = {
+    # A. Các Trường thành viên (School Entity)
+    "SCHOOLS": {
+        "sict_corpus", "seee_corpus", "smae_corpus"
+    },
+
+    # B. Ngành đào tạo (Major) -> Để sinh viên tìm môn học, đồ án
+    "MAJORS": {
+        "cnkt_ddt", "cnkt_dtvt", "cnkt_dtys", "cnkt_mt", "cnkt_nhiet", "ktsxtm",
+        "cnkt_dktdh", "nltt", "antoanthongtin", "congnghedaphuongtien",
+        "hethongthongtin", "kithuatphanmem", "congnghethongtin", "khoahocmanhtinh",
+        "cnkt_cdt", "cnkt_codientuoto", "cnkt_htcn", "cnkt_km", "cnkt_ck",
+        "cnkt_oto", "robot&ttnt", "tkck&dkcn", "kt_ckdl"
+    },
+
+    # C. Các Đơn vị hành chính/Khoa quản lý (Department/Faculty/Center)
+    "DEPARTMENTS": {
+        # Các Khoa
+        "khoa_congnghephanmem", "khoa_congnghethongtin", "khoa_khoahocmaytinh",
+        "khoa_mangmaytinhvatruyenthong", "kd", "kdt", "khtn&vm", "kktnl", "tdh",
+        # Các Trung tâm/Phòng
+        "hanhchinhtonghop", "phongdaotaokhcncongtacsinhvien", "tt_dt_boiduongvahoptacphattrien",
+        "tt_nghiencuucongnghesxtm", "tt_thuchanhvathucnghiemcokhi", "tt_thuchanhvathucnghiemoto",
+        "tt_hoptacphattrien", "tt_nc&ungdungttnt", "tt_nc_cgcn_ddt", "tt_th_ddt",
+        "bgh", "dubp", "hdkhdt", "tochucdoanthe"
+    },
+
+    # D. Bậc học (Level)
+    "LEVELS": {
+        "daihoc", "saudaihoc", "thacsi", "tiensi", 
+        "bangtienanh", "daotaonganhan", "kysu", "daotaolienket"
+    },
+    
+    # E. Các danh mục chung (Category)
+    # (Những key nào không nằm trong 4 nhóm trên sẽ tự động được coi là Category hoặc Topic)
+}
